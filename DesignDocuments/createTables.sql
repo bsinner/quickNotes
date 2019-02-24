a698b95a968c5c367c0a52e0f213abc9373365d6
Blake Sinner <BSINNER@madisoncollege.edu>
2019-02-24 03:25:59 -0600

    Add create tables script

---
+++ DesignDocuments/createTables.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY
+  , username varchar(30) NOT NULL UNIQUE
+  , email varchar(100) NOT NULL UNIQUE
+  , password varchar(25) NOT NULL
+);

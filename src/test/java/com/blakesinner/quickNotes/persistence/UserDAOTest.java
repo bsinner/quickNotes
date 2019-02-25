ed1c38738cb8d3d5523c0e6504b035a49fdfdeeb
Blake Sinner <BSINNER@madisoncollege.edu>
2019-02-25 03:45:21 -0600

    Add stubed UserDAO test, fix pom.xml

---
+++ src/test/java/com/blakesinner/quickNotes/persistence/UserDAOTest.java
@@ -0,0 +1,21 @@
+package com.blakesinner.quickNotes.persistence;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getUsersByUsername() {
+    }
+}

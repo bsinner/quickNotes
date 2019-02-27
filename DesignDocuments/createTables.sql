4fb07f8e96aaf1a66f8bd2fd1bb1a79dd2b9743a
Blake Sinner <BSINNER@madisoncollege.edu>
2019-02-27 03:14:13 -0600

    Update createTables to create a notes table

--- DesignDocuments/createTables.sql
+++ DesignDocuments/createTables.sql
@@ -1,8 +1,17 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, notes;
 
 CREATE TABLE users (
-  id int AUTO_INCREMENT PRIMARY KEY
-  , username varchar(30) NOT NULL UNIQUE
-  , email varchar(100) NOT NULL UNIQUE
-  , password varchar(25) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY
+    , username VARCHAR(30) NOT NULL UNIQUE
+    , email VARCHAR(100) NOT NULL UNIQUE
+    , password VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE notes (
+    id INT AUTO_INCREMENT PRIMARY KEY
+    , user_id INT NOT NULL
+    , title VARCHAR(40)
+    , contents TEXT
+    , creation_date DATETIME DEFAULT CURRENT_TIMESTAMP
+    , CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users(id)
 );

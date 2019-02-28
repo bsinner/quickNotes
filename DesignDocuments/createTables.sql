b38b249498f5c20f04f7289c55f2c88804655ced
Blake Sinner <BSINNER@madisoncollege.edu>
2019-02-28 00:45:07 -0600

    Update create tables to include sample data

--- DesignDocuments/createTables.sql
+++ DesignDocuments/createTables.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS users, notes;
+DROP TABLE IF EXISTS notes;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY
@@ -13,5 +14,20 @@ CREATE TABLE notes (
     , title VARCHAR(40)
     , contents TEXT
     , creation_date DATETIME DEFAULT CURRENT_TIMESTAMP
-    , CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users(id)
+    , CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+INSERT INTO users
+    (id, username, email, password)
+VALUES
+    (1, "bsmith", "bsmith@gmail.com", "password1")
+    , (2, "zsmith", "zsmith@gmail.com", "password2")
+    , (3, "ldavis", "ldavis@gmail.com", "password35");
+
+INSERT INTO notes
+    (user_id, title, contents)
+VALUES
+    (2, "a", "aa")
+    , (2, "b", "bb")
+    , (3, "c", "cc")
+;

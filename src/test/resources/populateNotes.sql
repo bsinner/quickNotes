20007df9aca9e24f80b916a9d5c567146b09c439
Blake Sinner <BSINNER@madisoncollege.edu>
2019-03-02 19:13:15 -0600

    Add setup sql for note tests

---
+++ src/test/resources/populateNotes.sql
@@ -0,0 +1,6 @@
+INSERT INTO notes
+    (id, user_id, title, contents)
+VALUES
+    (1, 2, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\n"}]}')
+    , (2, 3, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\n"}]}')
+    , (3, 1, "Lorem Ipsum", '{"ops":[{"attributes":{"italic":true},"insert":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"insert":"\\n"}]}');

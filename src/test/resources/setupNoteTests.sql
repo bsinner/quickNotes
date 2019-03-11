f9e16ff39b864f6c47a0cc5f9fd2297650440006
Blake Sinner <BSINNER@madisoncollege.edu>
2019-03-10 23:55:57 -0500

    Update newlines in sql files

--- src/test/resources/setupNoteTests.sql
+++ src/test/resources/setupNoteTests.sql
@@ -3,6 +3,6 @@ DELETE FROM notes;
 INSERT INTO notes
     (id, user_id, title, contents)
 VALUES
-    (1, 2, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\n"}]}')
-    , (2, 3, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\n"}]}')
-    , (3, 3, "Lorem Ipsum", '{"ops":[{"attributes":{"italic":true},"insert":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"insert":"\\n"}]}');
+    (1, 2, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\\\n"}]}')
+    , (2, 3, "Hello World Note", '{"ops":[{"insert":"hello "},{"attributes":{"bold":true},"insert":"world"},{"insert":"\\\\n"}]}')
+    , (3, 3, "Lorem Ipsum", '{"ops":[{"attributes":{"italic":true},"insert":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."},{"insert":"\\\\n"}]}');

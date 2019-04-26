96b02fc7e4fbc4f42f076ebf55c98ef8f822babd
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-25 19:01:42 -0500

    Remove uneeded import statments from Editor servlet

--- src/main/java/com/blakesinner/quickNotes/controller/Editor.java
+++ src/main/java/com/blakesinner/quickNotes/controller/Editor.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(
         urlPatterns = ("/editor")

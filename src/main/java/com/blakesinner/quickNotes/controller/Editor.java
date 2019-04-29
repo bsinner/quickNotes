9f179123dacd7f0fe7a5ef659debd1d74a91294f
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-29 01:18:12 -0500

    Add note loading functionality to editor servlet

--- src/main/java/com/blakesinner/quickNotes/controller/Editor.java
+++ src/main/java/com/blakesinner/quickNotes/controller/Editor.java
@@ -1,5 +1,8 @@
 package com.blakesinner.quickNotes.controller;
 
+import com.blakesinner.quickNotes.entity.Note;
+import com.blakesinner.quickNotes.persistence.GenericDAO;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,12 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @WebServlet(
         urlPatterns = ("/editor")
 )
 
 public class Editor extends HttpServlet {
+
+    private static final Pattern PATTERN = Pattern.compile("note=[0-9]+");
+    private static final String PARAM = "note=";
 
     /** Forwards to the editor page and passes it an note if a note id was included in the query string.
      *
@@ -23,9 +32,42 @@ public class Editor extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String qString = req.getQueryString();
+
+        if (qString != null) {
+            String contents = getContents(qString);
+            if (contents != null) {
+                req.setAttribute("contents", contents);
+            }
+        }
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/editor.jsp");
         dispatcher.forward(req, res);
+    }
+
+    private String getContents(String qString) {
+        String id = parseId(qString);
+
+        if (id != null) {
+            GenericDAO<Note> dao = new GenericDAO<>(Note.class);
+            List<Note> results = dao.getByPropertyEqual("id", id);
+
+            if (results.size() > 0) {
+                return results.get(0).getContents();
+            }
+        }
+
+        return null;
+    }
+
+    private String parseId(String qString) {
+        Matcher matcher = PATTERN.matcher(qString);
+
+        if (matcher.find()) {
+            return matcher.group(0).replace(PARAM, "");
+        }
+
+        return null;
     }
 
 }

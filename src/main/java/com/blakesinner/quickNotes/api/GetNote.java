e75b62c4446f3b2d5bb0ad72fc095fd5582e0c48
Blake Sinner <BSINNER@madisoncollege.edu>
2019-04-29 15:17:10 -0500

    Delete blank line in GetNote

--- src/main/java/com/blakesinner/quickNotes/api/GetNote.java
+++ src/main/java/com/blakesinner/quickNotes/api/GetNote.java
@@ -4,7 +4,6 @@ import com.blakesinner.quickNotes.entity.Note;
 import com.blakesinner.quickNotes.entity.User;
 import com.blakesinner.quickNotes.persistence.GenericDAO;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.logging.log4j.LogManager;

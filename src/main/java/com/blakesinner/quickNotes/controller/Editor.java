b3f77bfa71f51f0db5b6700c0d8812c94b473b8b
Blake Sinner <BSINNER@madisoncollege.edu>
2019-05-02 13:35:39 -0500

    Remove unused instance variables from Editor servlet

--- src/main/java/com/blakesinner/quickNotes/controller/Editor.java
+++ src/main/java/com/blakesinner/quickNotes/controller/Editor.java
@@ -7,16 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 @WebServlet(
         urlPatterns = ("/editor")
 )
 
 public class Editor extends HttpServlet {
-
-    private static final Pattern PATTERN = Pattern.compile("note=[0-9]+");
-    private static final String PARAM = "note=";
 
     /** Forwards to the editor page and passes it an note if a note id was included in the query string.
      *

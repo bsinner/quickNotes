d4d019585c1ead87642511fbe9c01f3292798e97
Blake Sinner <blakesinner31@gmail.com>
2019-05-10 19:30:13 -0700

    Add javadoc to CreateNote

--- src/main/java/com/blakesinner/quickNotes/api/CreateNote.java
+++ src/main/java/com/blakesinner/quickNotes/api/CreateNote.java
@@ -3,8 +3,7 @@ package com.blakesinner.quickNotes.api;
 import com.blakesinner.quickNotes.entity.Note;
 import com.blakesinner.quickNotes.entity.User;
 import com.blakesinner.quickNotes.persistence.GenericDAO;
-
-import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -15,6 +14,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * Endpoint to create note
+ *
+ * @author bsinner
+ */
 @Secured(roles = {"USER", "ADMIN"})
 @Path("/create")
 public class CreateNote {
@@ -23,8 +27,13 @@ public class CreateNote {
     private SecurityContext securityContext;
     private final static GenericDAO<Note> dao = new GenericDAO<>(Note.class);
 
-    // TODO: change to put
-    @GET
+    /**
+     * Create note.
+     *
+     * @param title the note title
+     * @return      response indicating success of note creation
+     */
+    @PUT
     public Response createNote(@QueryParam("title") String title) {
 
         if (duplicateCheck(title)) {
@@ -36,6 +45,12 @@ public class CreateNote {
         return note != null ? okResponse(note.getId()) : serverErrorResponse();
     }
 
+    /**
+     * Check if the current user already has a note with the same title
+     *
+     * @param title the note title
+     * @return      true if a duplicate is found, false otherwise
+     */
     private boolean duplicateCheck(String title) {
         Set<Note> allNotes = getCurrentUser().getNotes();
 
@@ -46,7 +61,12 @@ public class CreateNote {
         return userNotes.size() != 0;
     }
 
-
+    /**
+     * Create a new note, add it to the database, and return the created note.
+     *
+     * @param title the note title
+     * @return      the created note
+     */
     private Note getNewNote(String title) {
         User user = getCurrentUser();
         Note note = new Note(title, "{}", user);
@@ -61,22 +81,48 @@ public class CreateNote {
         return newNotes.get(0);
     }
 
+    /**
+     * Response to send if the user has a note with the given title.
+     *
+     * @param title the title of the note to create
+     * @return      the error response
+     */
     private Response duplicateResponse(String title) {
         return Response.status(422)
                 .entity("Error 422: Note with title " + title + " already exists")
                 .build();
     }
 
+    /**
+     * Response to send if the server is unable to add a new note.
+     *
+     * @return the error response
+     */
     private Response serverErrorResponse() {
         return Response.status(500).entity("Error 500: Note could not be created").build();
     }
 
+    /**
+     * Response to send if a note is created successfully.
+     *
+     * @param id the added note id
+     * @return   Ok response with the added note id
+     */
     private Response okResponse(int id) {
         return Response.status(200).entity(id).build();
     }
 
+    /**
+     * Get the user making the current request.
+     *
+     * @return the current user
+     */
     private User getCurrentUser() {
-        return new GenericDAO<>(User.class).getByPropertyEqual("id", securityContext.getUserPrincipal().getName()).get(0);
+        return new GenericDAO<>(User.class)
+                .getByPropertyEqual(
+                        "id"
+                        , securityContext.getUserPrincipal().getName())
+                .get(0);
     }
 
 }

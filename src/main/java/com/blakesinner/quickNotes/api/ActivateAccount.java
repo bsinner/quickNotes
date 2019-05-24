4a96a634ad707949c52fdd18fac06a1f800c3a36
Blake Sinner <blakesinner31@gmail.com>
2019-05-24 16:42:03 -0500

    Add working ActivateAccount endpoint

--- src/main/java/com/blakesinner/quickNotes/api/ActivateAccount.java
+++ src/main/java/com/blakesinner/quickNotes/api/ActivateAccount.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.Response;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Path("activate")
@@ -37,6 +38,31 @@ public class ActivateAccount {
         if (tokens.size() < 1) {
             return Response.status(404).entity("Token not Found").build();
         }
-        return null;
+
+        ActivationToken token = tokens.get(0);
+
+        if (isExpired(token)) {
+            return Response.status(410).entity("Token expired").build();
+        }
+
+        User user = token.getUser();
+        user.setActivated(true);
+
+        userDao.saveOrUpdate(user);
+
+        List<User> updatedUsers = userDao
+                .getByPropertyEqual("id", String.valueOf(user.getId()));
+
+        if (updatedUsers.size() < 1 || !updatedUsers.get(0).isActivated()) {
+            return Response.status(500).entity("Could not activate user").build();
+        }
+
+        // TODO: delete token on success
+        
+        return Response.ok("Account activated").build();
+    }
+
+    private boolean isExpired(ActivationToken token) {
+        return LocalDateTime.now().isAfter(token.getExpireDate());
     }
 }

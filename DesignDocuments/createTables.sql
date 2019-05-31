f75d95acfb49d6a8cf2dbf6b416eb9179875e19b
bsinner <35578820+bsinner@users.noreply.github.com>
2019-05-31 14:43:17 -0500

    Delete blank lines in createTables.sql

--- DesignDocuments/createTables.sql
+++ DesignDocuments/createTables.sql
@@ -1,7 +1,5 @@
 DROP TABLE IF EXISTS user_roles, activation_tokens, notes, users;
 
-
-
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
     , username VARCHAR(30) NOT NULL UNIQUE

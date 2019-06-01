e1a817c49e29dec9974ceaf8ccf45828e7a9b400
Blake Sinner <blakesinner31@gmail.com>
2019-06-01 01:15:19 -0500

    Fix bug that made it impossible to log out on AWS

--- src/main/java/com/blakesinner/quickNotes/api/Logout.java
+++ src/main/java/com/blakesinner/quickNotes/api/Logout.java
@@ -3,8 +3,10 @@ package com.blakesinner.quickNotes.api;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * Log the user out.
@@ -13,20 +15,37 @@ import javax.ws.rs.core.Response;
 public class Logout {
 
     // TODO: revoke access token in the database
+    private static final String BASE_PATH = "/api/";
 
     /**
      * Log the user out by deleting the cookie that stores the access token.
      *
-     * @return the response
+     * @param uri the uri info, needed because the cookie path changes between
+     *            localhost and live host
+     * @return    the response
      */
     @POST
     @Produces("{text/plain}")
-    public Response logout() {
+    public Response logout(@Context UriInfo uri) {
         return Response.status(Response.Status.OK)
                 .header(HttpHeaders.SET_COOKIE
-                        , "access_token=deleted-cookie; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/quickNotes_war")
+                        , "access_token=deleted-cookie; "
+                                + "Expires=Thu, 01 Jan 1970 00:00:00 GMT; "
+                                + "Path=" + getPath(uri))
                 .entity("")
                 .build();
+    }
+
+    /**
+     * Get the base path of the application.
+     *
+     * @param uri the uri info
+     * @return    the application base path
+     */
+    private String getPath(UriInfo uri) {
+        String[] paths = uri.getBaseUri().toString().split("/");
+
+        return "/" + paths[paths.length - 2];
     }
 
 }

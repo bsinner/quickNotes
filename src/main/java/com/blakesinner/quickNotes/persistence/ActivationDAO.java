9b05be326fca5c967270edfea0683815e69423ad
Blake Sinner <blakesinner31@gmail.com>
2019-06-01 18:09:59 -0500

    Add public activate user method to DAO

--- src/main/java/com/blakesinner/quickNotes/persistence/ActivationDAO.java
+++ src/main/java/com/blakesinner/quickNotes/persistence/ActivationDAO.java
@@ -1,25 +1,31 @@
 package com.blakesinner.quickNotes.persistence;
 
 import com.blakesinner.quickNotes.entity.ActivationToken;
+import com.blakesinner.quickNotes.entity.User;
+import com.blakesinner.quickNotes.entity.UserRole;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import sun.net.www.content.text.Generic;
 
+import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
- * DAO with activation token specific functionality.
+ * DAO for setting up activation tokens, and using them to activate users.
  *
  * @author bsinner
  */
-public class ActivationTokenDAO extends GenericDAO<ActivationToken> {
+public class ActivationDAO extends GenericDAO<ActivationToken> {
 
     private final SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
     /**
-     * No argument constructor for ActivationTokenDAO.
+     * No argument constructor for ActivationDAO.
      */
-    public ActivationTokenDAO() {
+    public ActivationDAO() {
         setType(ActivationToken.class);
     }
 
@@ -40,6 +46,33 @@ public class ActivationTokenDAO extends GenericDAO<ActivationToken> {
 
         session.close();
         return id;
+    }
+
+    public User activateUser(ActivationToken token) {
+        User user = token.getUser();
+
+        User activatedUser = updateUser(user);
+        GenericDAO<User> userDao = new GenericDAO<>(User.class);
+
+        userDao.saveOrUpdate(activatedUser);
+        List<User> results = userDao
+                .getByPropertyEqual("id", String.valueOf(activatedUser.getId()));
+
+        super.delete(token);
+
+        return results.isEmpty() ? null : results.get(0);
+    }
+
+    private User updateUser(User user) {
+        Set<UserRole> roles = user.getUserRoles();
+        roles.add(new UserRole("USER"));
+
+        Set<UserRole> updatedRoles = roles.stream()
+                .filter(r -> !r.getRole().equals("UNACTIVATED"))
+                .collect(Collectors.toSet());
+
+        user.setUserRoles(updatedRoles);
+        return user;
     }
 
 

a4b21cc31be726b3575e44b982b1238038054c32
Blake Sinner <blakesinner31@gmail.com>
2019-06-01 21:49:23 -0500

    Add send activation email utility class

---
+++ src/main/java/com/blakesinner/quickNotes/util/ActivationEmail.java
@@ -0,0 +1,62 @@
+package com.blakesinner.quickNotes.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.ws.rs.core.UriInfo;
+import java.util.Properties;
+
+import static javax.mail.Message.RecipientType.TO;
+
+public class ActivationEmail {
+
+    private static final Logger LOGGER = LogManager.getLogger(ActivationEmail.class);
+    private static final String BASE_PATH = "/api/";
+
+    private static void send(String token, UriInfo uri, String dest) {
+        PropertiesLoader loader = new PropertiesLoader();
+        Properties mailProps = loader.load("/mail.properties");
+        Properties authProps = loader.load("/mailLogin.properties");
+
+        Session mailSession = Session.getInstance(mailProps, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(
+                        authProps.getProperty("user"), authProps.getProperty("pass")
+                );
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(mailSession);
+            message.setFrom(new InternetAddress(authProps.getProperty("user")));
+            message.setRecipient(TO, new InternetAddress(dest));
+            message.setSubject("Confirm Account");
+            message.setContent(getBody(token, uri), "text/html");
+
+        } catch (MessagingException me) {
+            LOGGER.trace(me);
+        }
+    }
+
+    private static String getBody(String token, UriInfo uri) {
+        String url = uri.getBaseUri().toString().replace(BASE_PATH, "")
+                + "/activate?t=" + token;
+
+        return "<div style=\"margin: 1em, 1em, 1em, 1em;\">"
+                + "<h3 style=\"font-family:Lato,'Helvetica Neue',Arial,Helvetica,sans-serif\">"
+                    + "Account Created&nbsp;"
+                + "</h3>"
+                + "<h5 style=\"font-family:Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;\">"
+                    + "Your Quick Notes account has been created, click here to activate."
+                + "</h5>"
+                + "<a class=\"ui button\" href=\"" + url + "\""
+                        + "style=8\"cursor: pointer;display: inline-block;min-height: 1em;outline: 0;border: none;vertical-align: baseline;background: #e0e1e2 none;color: rgba(0,0,0,.6);font-family: Lato,'Helvetica Neue',Arial,Helvetica,sans-serif;margin: 0 .25em 0 0;padding: .78571429em 1.5em .78571429em;font-weight: 700;line-height: 1em;font-style: normal;text-align: center;border-radius: .28571429rem;margin-left: auto; margin-right: auto;\">"
+                    + "Confirm Account"
+                + "</a>"
+                + " </div>";
+    }
+}

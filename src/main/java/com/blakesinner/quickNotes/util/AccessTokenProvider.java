f74f851e77f2915481fb3ea05c73934a48f7242f
Blake Sinner <blakesinner31@gmail.com>
2019-06-04 01:51:08 -0500

    Add javadoc to AccessTokenProvider

--- src/main/java/com/blakesinner/quickNotes/util/AccessTokenProvider.java
+++ src/main/java/com/blakesinner/quickNotes/util/AccessTokenProvider.java
@@ -1,22 +1,31 @@
 package com.blakesinner.quickNotes.util;
 
 import com.blakesinner.quickNotes.entity.User;
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Create an access token set to expire in one hour.
+ *
+ * @author bsinner
+ */
 public class AccessTokenProvider {
 
     private static final int LIFESPAN = 60 * 60000;
     private static final String ISSUER = "Quick Notes";
     private static final String KEY_PATH = "/accessTokenPw.txt";
 
+    /**
+     * Get the access token string.
+     *
+     * @param user the user
+     * @return     the string
+     */
     public static String get(User user) {
         Date expiry = new Date();
         expiry.setTime(expiry.getTime() + LIFESPAN);
@@ -35,8 +44,6 @@ public class AccessTokenProvider {
                         Keys.hmacShaKeyFor(new KeyLoader().getKeyBytes(KEY_PATH))
                         , SignatureAlgorithm.HS256)
                 .compact();
-
-
     }
 
 }

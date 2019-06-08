062c0256de67e08fdcd3fa616619210b2270080b
Blake Sinner <blakesinner31@gmail.com>
2019-06-07 20:53:07 -0500

    Add javadoc to AccessTokenProvider

--- src/main/java/com/blakesinner/quickNotes/util/AccessTokenProvider.java
+++ src/main/java/com/blakesinner/quickNotes/util/AccessTokenProvider.java
@@ -38,10 +38,14 @@ public class AccessTokenProvider {
     public String createAccessToken(User user) { return createAccessJwt(user); }
 
     /**
-     * Create access token string.
+     * Create access token from a refresh token, if the refresh token is expired
+     * delete it and return null.
      *
-     * @param refreshTokenId the refresh token id
-     * @return the string
+     * @param refreshTokenId the refresh ID, used instead of a RefreshToken
+     *                       so the caller doesn't need to find the token in
+     *                       DB and check for expiration
+     * @return               the access token JWT string, or null if the refresh
+     *                       token is expired or not found
      */
     public String createAccessToken(String refreshTokenId) {
         GenericDAO<RefreshToken> dao = new GenericDAO<>(RefreshToken.class);
@@ -58,10 +62,10 @@ public class AccessTokenProvider {
     }
 
     /**
-     * Create refresh token string.
+     * Create a refresh token.
      *
-     * @param user the user
-     * @return the string
+     * @param user the refresh token owner
+     * @return     the token ID
      */
     public String createRefreshToken(User user) {
         GenericDAO<RefreshToken> dao = new GenericDAO<>(RefreshToken.class);
@@ -71,27 +75,35 @@ public class AccessTokenProvider {
     }
 
     /**
-     * Access cookie string string.
+     * Create an HTTP Set Cookie string containing an access token.
      *
-     * @param token the token
-     * @param path  the path
-     * @return the string
+     * @param token the access token
+     * @param path  the path, needed because default may be base path
+     *              of the Jersey servlet
+     * @return      the cookie string
      */
     public String accessCookieString(String token, String path) {
         return cookieString(ACCESS_NAME, token, path);
     }
 
     /**
-     * Refresh cookie string string.
+     * Create an HTTP Set Cookie string containing a refresh token.
      *
-     * @param token the token
-     * @param path  the path
-     * @return the string
+     * @param token the refresh token
+     * @param path  the path, needed because default may be base path
+     *              of the Jersey servlet
+     * @return      the cookie string
      */
     public String refreshCookieString(String token, String path) {
         return cookieString(REFRESH_NAME, token, path);
     }
 
+    /**
+     * Create a JWT access token.
+     *
+     * @param user the token owner
+     * @return     the token string
+     */
     private String createAccessJwt(User user) {
         Date expiry = new Date();
         expiry.setTime(expiry.getTime() + LIFESPAN);
@@ -112,12 +124,26 @@ public class AccessTokenProvider {
                 ).compact();
     }
 
+    /**
+     * Get an HTTP Set Cookie string for a given token.
+     *
+     * @param name  the type of token
+     * @param token the token string
+     * @param path  the applications base path
+     * @return      the cookie string
+     */
     private String cookieString(String name, String token, String path) {
         return name + "=" + token
                 + "; Path=" + path
                 + "; HttpOnly";
     }
 
+    /**
+     * Delete refresh token from DB.
+     *
+     * @param token the token to delete
+     * @param dao   a GenericDAO set to access refresh tokens
+     */
     private void delete(RefreshToken token, GenericDAO<RefreshToken> dao) {
         String id = token.getId().toString();
         dao.delete(token);
@@ -127,6 +153,13 @@ public class AccessTokenProvider {
         }
     }
 
+    /**
+     * Get if refresh token is expired.
+     *
+     * @param token the refresh token
+     * @return      true if the token is expired,
+     *              false if it's valid
+     */
     private boolean isExpired(RefreshToken token) {
         return token.getExpireDate().isBefore(LocalDateTime.now());
     }
